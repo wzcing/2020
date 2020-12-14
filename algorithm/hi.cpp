7bf9291035132e50245fcf82ccbcd26b4cd932d4
吴子辰 <wzcing@outlook.com>
2020-12-14 18:20:13 +0800

    实验1完成

--- algorithm/hi.cpp
+++ algorithm/hi.cpp
@@ -20,6 +20,36 @@ void merge(int *a,int *l,int left,int *r,int right){
         a[k++] = r[j++];
 }
 
+void sort(int *a,int n){
+    int mid, i, *l, *r;
+    if(n<2)
+        return;
+    mid = n / 2;
+    l = new int[mid];
+    r = new int[n - mid];
+    for (int i = 0; i < mid;i++){
+        l[i] = a[i];
+    }
+    for (int i = mid; i < n;i++){
+        r[i - mid] = a[i];
+    }
+    sort(l, mid);
+    sort(r, n - mid);
+    merge(a, l, mid, r, n - mid);
+    delete[] r;
+    delete[] l;
+}
+
+int main(){
+    int a[10] = {6, 2, 3, 1, 9, 10, 15, 13, 12, 17};
+    sort(a, 10);
+    for (int i = 0; i < 10;i++)
+        cout << a[i] << " ";
+}
+
+
+
+
 
 
 
